9c3a114fec44bd928dbbb18cbfdee6187a7f11bd
Yann Moisan <yamo93@gmail.com>
2011-12-15 02:24:10 +0100

    Refactor with an enum

--- src/com/yannmoisan/foobarqix/Main.java
+++ src/com/yannmoisan/foobarqix/Main.java
@@ -1,51 +1,60 @@
 package com.yannmoisan.foobarqix;
 
 public class Main {
-
-	private static final String[] SUBSTITUTION_WORDS = new String[] { "Foo", "Bar", "Qix" };
-	private static final char[] DIGITS = { '3', '5', '7' };
-	private static final int[] DIVISORS = new int[] { 3, 5, 7 };
-	
-	private static final int MAX_INDEX = 100;
-
 	public static void main(String[] args) {
-		for (int i = 1; i <= MAX_INDEX; i++) {
+		for (int i = 1; i <= 100; i++) {
 			System.out.println(process(i));
 		}
 	}
 
 	/* package*/ static String process(int n) {
-		StringBuilder sb = new StringBuilder();
+		final StringBuilder sb = new StringBuilder();
 
-		processDivisor(sb, n);
-		processContent(sb, n);
-		processEmpty(sb, n);
+		processStepDivisor(sb, n);
+		processStepContent(sb, n);
+		processStepEmpty(sb, n);
 
 		return sb.toString();
 	}
 	
-	private static void processDivisor(StringBuilder sb, int n) {
-		for (int i = 0; i < SUBSTITUTION_WORDS.length; i++) {
-			if (n % DIVISORS[i] == 0) {
-				sb.append(SUBSTITUTION_WORDS[i]);
+	private static void processStepDivisor(StringBuilder sb, int n) {
+		for (FooBarQix fbq:FooBarQix.values()) {
+			if (n % fbq.value == 0) {
+				sb.append(fbq.replace);
 			}
 		}
 	}
 
-	private static void processContent(StringBuilder sb, int n) {
+	private static void processStepContent(StringBuilder sb, int n) {
 		String s = Integer.toString(n);
 		for (int j = 0; j < s.length(); j++) {
-			for (int k = 0; k < SUBSTITUTION_WORDS.length; k++) {
-				if (s.charAt(j) == DIGITS[k]) {
-					sb.append(SUBSTITUTION_WORDS[k]);
+			for (FooBarQix fbq:FooBarQix.values()) {
+				if (s.charAt(j) == fbq.charValue) {
+					sb.append(fbq.replace);
 				}
 			}
 		}
 	}
 
-	private static void processEmpty(StringBuilder sb, int n) {
+	private static void processStepEmpty(StringBuilder sb, int n) {
 		if (sb.length() == 0) {
 			sb.append(n);
 		}
 	}	
+	
+	private enum FooBarQix {
+		FOO("Foo", 3),
+		BAR("Bar", 5),
+		QIX("Qix", 7);
+		
+		private final String replace;
+		private final int value;
+		private final char charValue;
+		
+		private FooBarQix(String replace, int value) {
+			this.replace = replace;
+			this.value = value;
+			this.charValue = Character.forDigit(value, 10);
+		}
+	}
 }
